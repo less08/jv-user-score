3e12f5ca8d32cc741f5e657e499ef06ad649b160
Elena Samsonova <elenasamsonova08@gmail.com>
2020-12-03 17:57:40 +0200

    user score task done

--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -1,8 +1,21 @@
 package core.basesyntax;
 
+import core.basesyntax.exception.UserNotFoundException;
+
 public class UserService {
+    private static final int EMAIL_INDEX = 0;
+    private static final int RECORD_INDEX = 1;
+    private static final String DIVIDER = ":";
 
     public int getUserScore(String[] records, String email) {
-        return 0;
+        for (String record : records) {
+            String[] recordRow = record.split(DIVIDER);
+            if (recordRow[EMAIL_INDEX].equals(email)) {
+                return Integer.parseInt(recordRow[RECORD_INDEX]);
+            }
+        }
+
+        throw new UserNotFoundException("User with given email doesn't exist");
     }
+
 }
